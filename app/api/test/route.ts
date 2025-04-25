6985e7a63fddec25ba6a414e7dd7c529db1e4d11
Hüseyin Çoraklı <huseyincorakli46@gmail.com>
2025-04-25 22:27:56 +0300

    fix

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -3,20 +3,6 @@ import { NextResponse } from "next/server";
 const serverIp = '43.80.54.62';
 
 export async function GET(request: Request) {
-    // 'X-Forwarded-For' başlığını al
-    const clientIp = request.headers.get('x-forwarded-for');
-    
-    // Eğer IP birden fazla IP içeriyorsa, ilkini alalım
-    const realIp = clientIp ? clientIp.split(',')[0] : '';
-  
-    // Sunucu IP'sini kontrol et
-    if (realIp === serverIp) {
-      return NextResponse.json({
-        message: 'Request accepted',
-        userIp: realIp, // Kullanıcının gerçek IP adresini burada alıyoruz
-      });
-    }
-  
-    // Eğer IP başka bir IP ise (sunucu dışında bir yerden geldiyse), reddediyoruz
-    return NextResponse.json({ error: 'Forbidden: Only requests from the server are allowed.' }, { status: 403 });
+    const ip = request.headers.get('x-forwarded-for')?.split(',')[0] ?? '127.0.0.1';
+    return NextResponse.json({ ip });
   }
