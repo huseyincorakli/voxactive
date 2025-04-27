c38fb66a319ea3f9484efa2e0d8d6aa1e9ae94b1
Hüseyin Çoraklı <huseyincorakli46@gmail.com>
2025-04-27 13:20:55 +0300

    metadata updated

--- app/(root)/learn/talk/page.tsx
+++ app/(root)/learn/talk/page.tsx
@@ -1,10 +1,30 @@
 import TalkAI from '@/components/TalkAI'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Learn English by speaking with AI',
+  description: 'Experience advanced AI conversations with our AI',
+  openGraph: {
+    title: 'Learn English by speaking with AI',
+    description: 'Experience advanced AI conversations with our AI',
+    url: 'https://voxactive.huscor.tech/learn/talk',
+    siteName: 'VoxActive',
+    images: [
+      {
+        url: 'https://voxactive.huscor.tech/ai-avatar.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  
+}
 
 const page = () => {
- 
-
   return (
-   <TalkAI/>
+    <TalkAI/>
   )
 }
 
